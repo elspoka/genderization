5a9158833da93c348478a7856f13a9fcc9cb4cc3
J. C <elspoka@hotmail.com>
2018-11-29 09:46:52 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // the function which handles the input field logic
 function getName() {
-    var nameField = document.getElementById('nameField').value;
-    var result = document.getElementById('result');
+    const nameField = document.getElementById('nameField').value;
+    const result = document.getElementById('result');
            
     if (nameField.length < 2) {
         result.textContent = 'Name must contain at least 2 characters';
@@ -12,10 +12,10 @@ function getName() {
 		//alert(nameField);
     }
     //AJAX - Server Response, 4: request finished and response is ready, 200: "OK"
-      var xmlhttp = new XMLHttpRequest();
+      const xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200)  {
-        var myObj = JSON.parse(this.responseText);
+        const myObj = JSON.parse(this.responseText);
         document.getElementById("gender").innerHTML = myObj.gender;
     }
 };
@@ -24,5 +24,5 @@ xmlhttp.send();
     
 }
 // use an eventlistener for the event
-var subButton = document.getElementById('subButton');
+const subButton = document.getElementById('subButton');
 subButton.addEventListener('click', getName);
