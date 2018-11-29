18f367507699f9aa2415d8e567ee2b29160ff84b
J. C <elspoka@hotmail.com>
2018-11-29 16:59:08 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ subButton.addEventListener('click', getName);
 const nameInput = document.getElementById('nameField');
 nameInput.addEventListener("keyup", function(event) {
   // Cancel the default action, if needed
-  event.preventDefault();
+  //event.preventDefault();
   // Number 13 is the "Enter" key on the keyboard
   if (event.keyCode === 13) {
     // Trigger the button element with a click
